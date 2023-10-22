466184157204017474f64722464fc405b4cc2abb
jcarlo <jcarlo@outlook.com>
2023-10-22 01:29:09 -0300

    mais organizado. animação do botão concertada

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,6 @@ const audioEncerramento = new Audio("./assets/audios/Beeper_Emergency_Call.mp3")
 
 const textoTempo = document.getElementById("texto-tempo");
 
-let mostrarControlesFullscreen = false;
-
 const informacoes = {
     horasRest: document.getElementById("horas"),
     minutosRest: document.getElementById("minutos"),
@@ -43,6 +41,35 @@ const controles = {
     text2Tela: document.getElementById("checker-2-tela")
 }
 
+let tempoRestante;
+let mostrarControlesFullscreen = false;
+
+// para a execução do código pra não mostrar tempo negativo
+let continuar = true;
+
+//enter nos inputs inicia o cronômetro
+campos.campoHoraTermino.addEventListener("keyup", (x) => {
+    if (x.key === "Enter") iniciarHoraTermino();
+});
+campos.campoTempoLimiteHora.addEventListener("keyup", (x) => {
+    if (x.key === "Enter") iniciarDuracao();
+});
+campos.campoTempoLimiteMinuto.addEventListener("keyup", (x) => {
+    if (x.key === "Enter") iniciarDuracao();
+});
+// fullscreens
+document.addEventListener("keydown", (x) => {
+    if (x.key === "f") {
+        funcoesFullscreen.fullscreen();
+    }
+    else if (x.key === "Escape") {
+        alternarBotoesFullScreen.visibBotoesFullScreen();
+    }
+});
+controles.zerar.addEventListener("click", () => {
+    location.reload();
+})
+
 const funcoesEletronJS = {
     mostrarMenu: () => {
         if (document.fullscreenElement && window.funcoesWinElectron) window.funcoesWinElectron.mostrarMenu();
@@ -62,120 +89,97 @@ const funcoesEletronJS = {
     }
 }
 
-let tempoRestante;
+const funcoesFullscreen = {
+    entrarFullscreen: function () {
 
-// para a execução do código pra não mostrar tempo negativo
-let continuar = true;
+        // coloca em tela cheia
+        let elem = document.documentElement;
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.webkitRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+        }
 
-//enter nos inputs inicia o cronômetro
-campos.campoHoraTermino.addEventListener("keyup", (x) => {
-    if (x.key === "Enter") iniciarHoraTermino();
-});
-campos.campoTempoLimiteHora.addEventListener("keyup", (x) => {
-    if (x.key === "Enter") iniciarDuracao();
-});
-campos.campoTempoLimiteMinuto.addEventListener("keyup", (x) => {
-    if (x.key === "Enter") iniciarDuracao();
-});
-// fullscreens
-document.addEventListener("keydown", (x) => {
-    if (x.key === "f") {
-        fullscreen();
+        // altera visibilidade dos controles em tela cheia
+        alternarBotoesFullScreen.mostrarBtnSairFullscreen();
+        controles.onOff2Tela.style.removeProperty("display");
+        controles.onOff2Tela.classList.add("ocultar");
+        controles.zerar.style.removeProperty("display");
+        controles.zerar.classList.add("ocultar");
+
+        funcoesEletronJS.paraSegundaTela();
+        funcoesEletronJS.ocultarMenu();
+
+    },
+    sairFullscreen: function () {
+
+        // sai da tela cheia
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.webkitExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+        }
+
+        // altera a visibilidade dos controles quando sai da tela cheia
+        alternarBotoesFullScreen.mostrarBtnIrFullscreen();
+        if (mostrarControlesFullscreen) {
+            controles.onOff2Tela.style.display = "flex";
+            controles.onOff2Tela.classList.remove("ocultar");
+            controles.zerar.style.display = "flex";
+            controles.zerar.classList.remove("ocultar");
+        }
+
+        funcoesEletronJS.sairSegundaTela();
+        funcoesEletronJS.mostrarMenu();
+    },
+    fullscreen: function () {
+
+        if (document.fullscreenElement) {
+            funcoesFullscreen.sairFullscreen();
+        } else {
+            funcoesFullscreen.entrarFullscreen();
+        }
+
     }
-    else if (x.key === "Escape") {
-        sairFullscreen();
-    }
-});
-controles.zerar.addEventListener("click", () => {
-    sairFullscreen();
-    location.reload();
-})
-
-function entrarFullscreen() {
-
-    // coloca em tela cheia
-    let elem = document.documentElement;
-    if (elem.requestFullscreen) {
-        elem.requestFullscreen();
-    } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-    }
-
-    // altera visibilidade dos controles em tela cheia
-    controles.botaoEntrarFullscreen.classList.add("ocultar");
-    controles.botaoSairFullscreen.classList.remove("ocultar");
-    controles.onOff2Tela.style.removeProperty("display");
-    controles.onOff2Tela.classList.add("ocultar");
-    controles.zerar.style.removeProperty("display");
-    controles.zerar.classList.add("ocultar");
-
-    funcoesEletronJS.paraSegundaTela();
-    funcoesEletronJS.ocultarMenu();
-
 }
 
-function sairFullscreen() {
-
-    // sai da tela cheia
-    if (document.exitFullscreen) {
-        document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) { /* Safari */
-        document.webkitExitFullscreen();
-    } else if (document.msExitFullscreen) { /* IE11 */
-        document.msExitFullscreen();
-    }
-
-    // altera a visibilidade dos controles quando sai da tela cheia
-    controles.botaoEntrarFullscreen.classList.remove("ocultar");
-    controles.botaoSairFullscreen.classList.add("ocultar");
-    if (mostrarControlesFullscreen) {
-        controles.onOff2Tela.style.display = "flex";
-        controles.onOff2Tela.classList.remove("ocultar");
-        controles.zerar.style.display = "flex";
-        controles.zerar.classList.remove("ocultar");
-    }
-
-    funcoesEletronJS.sairSegundaTela();
-    funcoesEletronJS.mostrarMenu();
-}
-
-function fullscreen() {
-
-    if (document.fullscreenElement) {
-        sairFullscreen();
-    } else {
-        entrarFullscreen();
-    }
-
-}
-
-// alterna a visibilidades dos botões de fullscreen
-async function visibBotoesFullScreen() {
-
-    if (!document.fullscreenElement) {
+const alternarBotoesFullScreen = {
+    mostrarBtnIrFullscreen: function () {
         controles.botaoEntrarFullscreen.classList.remove("ocultar");
         controles.botaoSairFullscreen.classList.add("ocultar");
-    } else {
+    },
+    mostrarBtnSairFullscreen: function () {
         controles.botaoEntrarFullscreen.classList.add("ocultar");
         controles.botaoSairFullscreen.classList.remove("ocultar");
+    },
+    // alterna a visibilidades dos botões de fullscreen
+    visibBotoesFullScreen: async function () {
+        if (document.fullscreenElement) {
+            alternarBotoesFullScreen.mostrarBtnSairFullscreen();
+        } else {
+            alternarBotoesFullScreen.mostrarBtnIrFullscreen()
+        }
     }
 }
 
 async function iniciarHoraTermino() {
 
-    visibBotoesFullScreen();
+    alternarBotoesFullScreen.visibBotoesFullScreen();
 
     if (campos.campoHoraTermino.value) {
         let horaTermino = campos.campoHoraTermino.value.split(":");
-        relogio([Number(horaTermino[0]), Number(horaTermino[1])]);
+        iniciarCronometro([Number(horaTermino[0]), Number(horaTermino[1])]);
     }
+
 }
 
 async function iniciarDuracao() {
 
-    visibBotoesFullScreen();
+    alternarBotoesFullScreen.visibBotoesFullScreen();
 
     if ((campos.campoTempoLimiteHora.value > 0 || campos.campoTempoLimiteMinuto.value > 0) &&
         (campos.campoTempoLimiteHora.value || campos.campoTempoLimiteMinuto.value)) {
@@ -192,16 +196,16 @@ async function iniciarDuracao() {
         let hora = Math.trunc(horarioLimite / 60);
         let minuto = horarioLimite % 60;
 
-        relogio([hora, minuto]);
+        iniciarCronometro([hora, minuto]);
 
     }
 
 }
 
-async function relogio(horaTermino) {
+async function iniciarCronometro(horaTermino) {
 
     // adiciona click duplo para colocar em tela cheia
-    document.querySelector("body").addEventListener("dblclick", fullscreen);
+    document.querySelector("body").addEventListener("dblclick", funcoesFullscreen.fullscreen);
 
     mostrarControlesFullscreen = true;
 
@@ -226,31 +230,12 @@ async function relogio(horaTermino) {
     controles.controles.style.height = "auto";
     informacoes.horario.forEach((x) => x.classList.remove("ocultar"));
 
-    let tempoRestanteInicial = new Date();
-
-    if (calculoTempoRestante(tempoRestanteInicial, horaTermino) <= 300) {
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.stopColor = "#00ff00";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#00ff00";
-    }
-
-    if (calculoTempoRestante(tempoRestanteInicial, horaTermino) <= 60) {
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.stopColor = "#ffff00";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#ffff00";
-    }
-
-    if (calculoTempoRestante(tempoRestanteInicial, horaTermino) <= 0) {
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.stopColor = "#ff0000";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#ff0000";
-    }
+    //atualizar cor de acordo com o tempo do início do cronômetro
+    let horaAtual = new Date();
+    animacoes.ajustarCores(horaAtual, horaTermino);
 
     while (true) {
-        let horaAtual;
+        horaAtual;
         let retorno = new Promise((resolve, reject) => {
             horaAtual = new Date();
             let restante = 1000 - horaAtual.getMilliseconds();
@@ -265,26 +250,7 @@ async function relogio(horaTermino) {
 
         await retorno;
     }
-}
 
-async function piscarDoisPontos() {
-    informacoes.doisPontos.forEach((x) => x.classList.remove("piscar_dois_pontos"));
-    setTimeout(() => {
-        if (tempoRestante > 0) {
-            informacoes.doisPontos.forEach((x) => x.classList.add("piscar_dois_pontos"));
-        }
-    }, 500);
-}
-
-function calculoTempoRestante(horas, horaTermino) {
-    let tempo = horas.getSeconds() + (horas.getMinutes() * 60) + (horas.getHours() * 3600);
-    let tempoTermino = (horaTermino[1] * 60) + (horaTermino[0] * 3600);
-
-    if (tempoTermino >= tempo) {
-        return tempoTermino - tempo;
-    } else {
-        return (tempoTermino + 86400) - tempo;
-    }
 }
 
 function imprimirRelogio(horaAtual, horaTermino) {
@@ -307,9 +273,9 @@ function imprimirRelogio(horaAtual, horaTermino) {
         if (minutosRestantes < 10) minutosRestantes = `0${minutosRestantes.toString()}`;
         if (segundosRestantes < 10) segundosRestantes = `0${segundosRestantes.toString()}`;
 
-        girarPonteiros(tempoRestante, segundosRestantes);
+        animacoes.girarPonteiros(tempoRestante, segundosRestantes);
 
-        reproduzirAudios();
+        animacoes.audioECores();
 
         if (horasRestantes == 0) {
             informacoes.horasRest.innerText = "";
@@ -322,52 +288,91 @@ function imprimirRelogio(horaAtual, horaTermino) {
         informacoes.minutosRest.innerText = `${minutosRestantes}`;
         informacoes.segundosRest.innerText = `${segundosRestantes}`;
 
-        piscarDoisPontos();
+        animacoes.piscarDoisPontos();
     }
 
     informacoes.horario[0].innerText = `Hora certa = ${horas}:${minutos}:${segundos}`;
 }
 
-async function girarPonteiros(tempoRestante) {
+function calculoTempoRestante(horas, horaTermino) {
+    let tempo = horas.getSeconds() + (horas.getMinutes() * 60) + (horas.getHours() * 3600);
+    let tempoTermino = (horaTermino[1] * 60) + (horaTermino[0] * 3600);
 
-    let a = tempoRestante / 10;
-    let b = tempoRestante * 6;
-
-    desenhoRelogio.ponteiroMinutos.style.transform = `rotate(${a}deg)`;
-    desenhoRelogio.ponteiroSegundos.style.transform = `rotate(${b}deg)`;
-}
-
-async function reproduzirAudios() {
-    if (tempoRestante >= 307 && tempoRestante <= 309) {
-        audio5minutos.play();
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.transition = "10s";
-            x.style.stopColor = "#00ff00";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#00ff00";
-        desenhoRelogio.corCirculoInterno.style.transition = "10s";
-    }
-
-    if (tempoRestante <= 68 && tempoRestante >= 66) {
-        audio1minuto.play();
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.transition = "10s";
-            x.style.stopColor = "#ffff00";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#ffff00";
-        desenhoRelogio.corCirculoInterno.style.transition = "10s";
-    }
-
-    if (tempoRestante <= 0 && tempoRestante >= -2) {
-        audioEncerramento.play();
-        desenhoRelogio.corCirculoExterno.forEach((x) => {
-            x.style.transition = "5s";
-            x.style.stopColor = "#ff0000";
-        });
-        desenhoRelogio.corCirculoInterno.style.fill = "#ff0000";
-        desenhoRelogio.corCirculoInterno.style.transition = "5s";
-        informacoes.textoLicao.innerText = "LIÇÃO DA ESCOLA SABATINA ENCERRADA";
-        continuar = false;
+    if (tempoTermino >= tempo) {
+        return tempoTermino - tempo;
+    } else {
+        return (tempoTermino + 86400) - tempo;
     }
 }
 
+const animacoes = {
+    ajustarCores: async function (horaAtual, horaTermino) {
+        if (calculoTempoRestante(horaAtual, horaTermino) <= 300) {
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.stopColor = "#00ff00";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#00ff00";
+        }
+        if (calculoTempoRestante(horaAtual, horaTermino) <= 60) {
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.stopColor = "#ffff00";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#ffff00";
+        }
+        if (calculoTempoRestante(horaAtual, horaTermino) <= 0) {
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.stopColor = "#ff0000";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#ff0000";
+        }
+    },
+    girarPonteiros: async function (tempoRestante) {
+
+        let a = tempoRestante / 10;
+        let b = tempoRestante * 6;
+
+        desenhoRelogio.ponteiroMinutos.style.transform = `rotate(${a}deg)`;
+        desenhoRelogio.ponteiroSegundos.style.transform = `rotate(${b}deg)`;
+    },
+    piscarDoisPontos: async function () {
+        informacoes.doisPontos.forEach((x) => x.classList.remove("piscar_dois_pontos"));
+        setTimeout(() => {
+            if (tempoRestante > 0) {
+                informacoes.doisPontos.forEach((x) => x.classList.add("piscar_dois_pontos"));
+            }
+        }, 500);
+    },
+    audioECores: async function () {
+        if (tempoRestante >= 307 && tempoRestante <= 309) {
+            audio5minutos.play();
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.transition = "10s";
+                x.style.stopColor = "#00ff00";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#00ff00";
+            desenhoRelogio.corCirculoInterno.style.transition = "10s";
+        }
+
+        if (tempoRestante <= 68 && tempoRestante >= 66) {
+            audio1minuto.play();
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.transition = "10s";
+                x.style.stopColor = "#ffff00";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#ffff00";
+            desenhoRelogio.corCirculoInterno.style.transition = "10s";
+        }
+
+        if (tempoRestante <= 0 && tempoRestante >= -2) {
+            audioEncerramento.play();
+            desenhoRelogio.corCirculoExterno.forEach((x) => {
+                x.style.transition = "5s";
+                x.style.stopColor = "#ff0000";
+            });
+            desenhoRelogio.corCirculoInterno.style.fill = "#ff0000";
+            desenhoRelogio.corCirculoInterno.style.transition = "5s";
+            informacoes.textoLicao.innerText = "LIÇÃO DA ESCOLA SABATINA ENCERRADA";
+            continuar = false;
+        }
+    }
+}
